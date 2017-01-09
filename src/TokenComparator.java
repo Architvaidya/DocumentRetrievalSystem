94682962b72100839c5d17c985e96ccfd32906ce
Architvaidya <archit.vaidya@gmail.com>
2017-01-09 05:39:23 -0500

    Adding TokenComparator.java

---
+++ src/TokenComparator.java
@@ -0,0 +1,19 @@
+package avaidya4_ykharwa1_part3;
+
+public class TokenComparator implements Comparable<Token>{
+
+	
+
+	@Override
+	public int compareTo(Token t2) {
+		/*String s1 = t1.name;
+		String s2 = t2.name;
+		
+		int flag = s1.compareTo(s2);
+		//System.out.println("Flag is: "+flag);
+		*/return 0;
+	}
+	
+
+}
+
