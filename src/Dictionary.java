dd23c61d9eb1079558e9851f2984abab2b510de5
Architvaidya <archit.vaidya@gmail.com>
2017-01-09 05:37:20 -0500

    Adding Dictionary.java

---
+++ src/Dictionary.java
@@ -0,0 +1,20 @@
+package avaidya4_ykharwa1_part3;
+public class Dictionary{
+ String Term;
+ int cf;
+ int df;
+ int offset;
+ static int start;
+ Dictionary(String term, int cf, int df, int offset){
+	this.Term = term;
+	this.cf = cf;
+	this.df = df;
+	this.offset = offset;
+}
+ 
+
+ 
+}
+
+
+
