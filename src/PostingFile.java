05d3fe66179389177895e1e2fa3b3c4c46492279
Architvaidya <archit.vaidya@gmail.com>
2017-01-09 05:38:08 -0500

    Adding PostingFile.java

---
+++ src/PostingFile.java
@@ -0,0 +1,14 @@
+package avaidya4_ykharwa1_part3;
+
+
+public class PostingFile {
+	String docName;
+	int tf;
+	
+	PostingFile(String docName, int tf){
+		this.docName = docName;
+		this.tf = tf;
+		
+	}
+
+}
